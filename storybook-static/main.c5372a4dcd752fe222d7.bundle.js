665436e90557456117c5605b6403a5b34c11b9c4
Mohamed Khan <khan@incube.sg>
2020-03-16 12:17:19 +0800

    Percy added

---
+++ storybook-static/main.c5372a4dcd752fe222d7.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],[function(n,o,p){p(405),n.exports=p(547)},function(n,o){}],[[0,1,2]]]);
---
+++ storybook-static/precache-manifest.48bd29037dd84416cce985c0fcc38c91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d160d9d5747ed356a8b91f3f7b9a4b6",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.d1fe3de09324b3597f02.bundle.js"
+  },
+  {
+    "url": "runtime~main.d1fe3de09324b3597f02.bundle.js"
+  },
+  {
+    "revision": "06db7adb003f664e981e",
+    "url": "static/css/main.21f59304.chunk.css"
+  },
+  {
+    "revision": "b3c22e38c72deaf47a8addc0d6ae1577",
+    "url": "static/media/toggle.b3c22e38.png"
+  },
+  {
+    "url": "vendors~main.d1fe3de09324b3597f02.bundle.js"
+  },
+  {
+    "url": "vendors~main.d1fe3de09324b3597f02.bundle.js.LICENSE.txt"
+  }
+]);
